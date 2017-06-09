0b6a78584c720e903ecd3db4cbb9e20a0de9d5fe
robflop <robflop98@gmail.com>
2017-06-09 17:01:13 +0200

    joining, leaving channels, debugging buttons etc

--- src/ressources/js/login.js
+++ src/ressources/js/login.js
@@ -15,22 +15,52 @@ $(document).ready(() => {
 			return e.preventDefault();
 		}
 
-		// e.preventDefault();
-
 		const serialized = $('#login').serializeArray();
+		const host = serialized[2].value.match(/^https?:\/\//) ? serialized[2].value : `http://${serialized[2].value}`;
 		user = { username: serialized[0].value, channels: serialized[1].value.split(', ') };
 		console.log(user, serialized[2].value);
 
-		socket = io.connect("http://"+serialized[2].value);
+		socket = io.connect(host);
 
 		socket.on('connect', () => {
 			socket.emit('join', user);
+			socket.on('duplicateUsernameError', error => {
+				null;
+			});
 			// window.location = '../../pages/index.html';
 		});
+
+		e.preventDefault(); // don't refresh the page
+	});
+
+	$('#logout').click(() => {
+		socket.emit('leave', user);
+		socket.disconnect();
+	});
+
+	function randomChannel() {
+		let name = '';
+		const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		for (let i = 0; i < 5; i++) {
+			name += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return name;
+	}
+
+	$('#joinChannel').click(() => {
+		const channel = randomChannel();
+		socket.emit('channelJoin', { user, channel });
+		user.channels.push(channel);
+	});
+
+	$('#leaveChannel').click(() => {
+		const channel = user.channels[Math.floor(Math.random() * user.channels.length)];
+		socket.emit('channelLeave', { user, channel });
+		user.channels.splice(user.channels.indexOf(channel), 1);
 	});
 
 	$(window).on('beforeunload', () => {
-		socket.emit('leave', { username: user.username });
+		socket.emit('leave', user);
 		socket.disconnect();
 	});
 });
