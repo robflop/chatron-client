954cbc9ad89fb70b98abbbe69d8498ae9aa41d65
robflop <robflop98@gmail.com>
2017-08-13 20:19:33 +0200

    things

--- src/client.js
+++ src/client.js
@@ -5,9 +5,8 @@ app.setName('Chatron Client');
 
 app.on('ready', () => {
 	const mainWindow = new BrowserWindow({});
-	const index = join('file://', __dirname, '/index.html');
-	mainWindow.loadURL(index);
-	mainWindow.webContents.openDevTools();
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	// mainWindow.webContents.openDevTools();
 
 	if (process.env.NODE_ENV !== 'production') {
 		require('vue-devtools').install();
