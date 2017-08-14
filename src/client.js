563cb47c62bd91c78a4f09d742e4d57ad3b650ed
robflop <robflop98@gmail.com>
2017-08-14 16:49:59 +0200

    finally got renderer process debugging working (?)

--- src/client.js
+++ src/client.js
@@ -6,7 +6,6 @@ app.setName('Chatron Client');
 app.on('ready', () => {
 	const mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
-	// mainWindow.webContents.openDevTools();
 
 	if (process.env.NODE_ENV !== 'production') {
 		require('vue-devtools').install();
