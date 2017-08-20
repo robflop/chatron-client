efa1e99c91fd855a77ba174308d67ad554bc7ad8
robflop <robflop98@gmail.com>
2017-08-20 23:10:59 +0200

    remove dev

--- src/client.js
+++ src/client.js
@@ -3,18 +3,13 @@ const { app, BrowserWindow } = require('electron');
 app.setName('Chatron Client');
 
 app.on('ready', () => {
-	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 720, center: true, show: false });
+	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 720, webPreferences: { devTools: false }, center: true, show: false });
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 	mainWindow.on('ready-to-show', () => {
 		mainWindow.setMenu(null);
 		mainWindow.setSize(1080, 720);
 		mainWindow.show();
 	});
-	mainWindow.webContents.openDevTools();
-
-	// if (process.env.NODE_ENV !== 'production') {
-	// 	require('vue-devtools').install();
-	// }
 });
 
 app.on('window-all-closed', () => app.quit());
