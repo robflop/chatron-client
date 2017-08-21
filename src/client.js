bf9a0f3f35ee8780a93281e8c5204ff9efcd2cae
robflop <robflop98@gmail.com>
2017-08-21 02:53:47 +0200

    Set title to window, not app

--- src/client.js
+++ src/client.js
@@ -1,13 +1,12 @@
 const { app, BrowserWindow } = require('electron');
 
-app.setName('Chatron Client');
-
 app.on('ready', () => {
 	const mainWindow = new BrowserWindow({ minWidth: 1080, minHeight: 720, webPreferences: { devTools: false }, center: true, show: false });
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
 	mainWindow.on('ready-to-show', () => {
 		mainWindow.setMenu(null);
 		mainWindow.setSize(1080, 720);
+		mainWindow.setTitle('Chatron');
 		mainWindow.show();
 	});
 });
